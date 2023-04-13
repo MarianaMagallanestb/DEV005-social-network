4203b031fce998000b3c83222a297c68441e62a5
Mariana MagallanesLMML <marianamagallanespp13@gmail.com>
2023-04-13 12:01:43 -0600

    Estilos

--- src/components/home.js
+++ src/components/home.js
@@ -4,6 +4,7 @@ function home() {
   img.src = 'https://fontmeme.com/permalink/230413/94187632b7e104d4a58cd78a1e645259.png';
   const paragraph = document.createElement('p');
   const btnCheckIn = document.createElement('button');
+  btnCheckIn.classList.add('mahahahahahaha');
   paragraph.textContent = '¡Comparte tu experiencia en confianza!';
   btnCheckIn.textContent = 'Resgistrarse';
   section.append(img, paragraph, btnCheckIn);
