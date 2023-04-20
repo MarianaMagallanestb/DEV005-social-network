23ced589c1078d5ea634b954145bc759360d3b90
Brina <brixiga1892@gmail.com>
2023-04-20 15:18:22 -0500

    firebase instalacio

--- src/configurar firebase/firebase.js
+++ src/configurar firebase/firebase.js
@@ -6,7 +6,7 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCLbSqZdX-nROzDUPmVUrTcm5d8dkunIfI',s
+  apiKey: 'AIzaSyCLbSqZdX-nROzDUPmVUrTcm5d8dkunIfI',
   autgithDomain: 'social-network-7ece4.firebaseapp.com',
   projectId: 'social-network-7ece4',
   storageBucket: 'social-network-7ece4.appspot.com',
