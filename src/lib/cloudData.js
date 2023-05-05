6c4de199f6d7fa04d201768bbbe13a073e775bb0
Mariana MagallanesLMML <marianamagallanespp13@gmail.com>
2023-05-04 18:04:01 -0600

    agregando post

--- src/lib/cloudData.js
+++ src/lib/cloudData.js
@@ -3,7 +3,7 @@ import { collection, addDoc, getDocs } from 'firebase/firestore';
 import { db } from '../configurar firebase/firebase.js';
 
 export const savePost = async (content) => {
- return await addDoc(collection(db, 'post'), { content });
+return await addDoc(collection(db, 'post'), { content });
 };
 
 export const getPost = () => getDocs(collection(db, 'post'));
